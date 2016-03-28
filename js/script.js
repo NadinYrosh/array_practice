def08a41b1af294723c2aaea5cbaeebe67b6e768
Erin Goncer and Nadiya Yeroshkina <erin@>
2016-03-28 11:24:03 -0700

    add .children.remove(); to update results everytime, instade of appendin it

--- js/script.js
+++ js/script.js
@@ -2,18 +2,14 @@ $(document).ready(function() {
   $("form").submit(function(event) {
   var inputs = ["food", "animal", "music", "color", "season", "activity"];
 
-// var nextInputs = inputs[1], inputs[0]
   var newArray = [];
     newArray.push(inputs[2], inputs[1], inputs[3]);
+    $("ul").children().remove();
+    newArray.forEach (function(input) {
 
-  newArray.forEach (function(input){
-    var userInput = $("#" + input).val();
-    $("ul#list").append("<li>" + userInput + "</li>");
-    console.log(userInput);
+  var userInput = $("#" + input).val();
+    $("ul").append("<li>" + userInput + "</li>");
   });
-
-
-    // var newArray.push(inputs[2]);
   event.preventDefault();
   });
 });
